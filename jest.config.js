be2be08bb54cfdef77dcd5ca81fc65b44e9a4a2c
christopher-estanis <christopher.estanis@gmail.com>
2022-07-07 20:46:14 -0300

    add lint-staged

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 module.exports = {
   collectCoverage: false,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
