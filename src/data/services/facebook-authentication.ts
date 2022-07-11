5178c083eae84064ecbd5cd13c71a06a905efbff
christopher-estanis <christopher.estanis@gmail.com>
2022-07-11 00:45:45 -0300

    ensure Service update account without overriding name

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -16,10 +16,10 @@ export class FacebookAuthenticationService {
 
     const accountData = await this.userAccountRepo.load({ email: fbData?.email })
 
-    if (accountData?.name !== undefined) {
+    if (accountData !== undefined) {
       await this.userAccountRepo.updateWithFacebook({
         id: accountData.id,
-        name: accountData.name,
+        name: accountData?.name ?? fbData.name,
         facebookId: fbData.facebookId
       })
     } else {
