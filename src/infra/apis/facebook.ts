4ee3150c5545d00b25562ac5ad4cbe9a5c1f799e
christopher-estanis <christopher.estanis@gmail.com>
2022-07-13 20:57:42 -0300

    ensure FacebookApi gets debug token

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -9,13 +9,21 @@ export class FacebookApi {
     private readonly clientSecret: string
   ) {}
 
-  async loadUser (Params: LoadFacebookUserApi.Params): Promise<void> {
-    await this.httpClient.get({
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
+    const appToken = await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this.clientId,
         client_secret: this.clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+
+    await this.httpClient.get({
+      url: `${this.baseUrl}/debug_token`,
+      params: {
+        access_token: appToken.access_token,
+        input_token: params.token
       }
     })
   }
