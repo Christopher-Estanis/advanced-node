85525bc80f2bf2b500aa4c5f0c35b5f1a67c3422
christopher-estanis <christopher.estanis@gmail.com>
2022-07-13 23:10:22 -0300

    improve code quality

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -1,6 +1,22 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { HttpGetClient } from '@/infra/http'
 
+type AppToken = {
+  access_token: string
+}
+
+type DebugToken = {
+  data: {
+    user_id: string
+  }
+}
+
+type UserInfo = {
+  id: string
+  name: string
+  email: string
+}
+
 export class FacebookApi implements LoadFacebookUserApi {
   private readonly baseUrl = 'https://graph.facebook.com'
   constructor (
@@ -10,7 +26,16 @@ export class FacebookApi implements LoadFacebookUserApi {
   ) {}
 
   async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
-    const appToken = await this.httpClient.get({
+    const userInfo = await this.getUserInfo(params.token)
+    return {
+      email: userInfo.email,
+      facebookId: userInfo.id,
+      name: userInfo.name
+    }
+  }
+
+  private async getAppToken (): Promise<AppToken> {
+    return this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this.clientId,
@@ -18,28 +43,29 @@ export class FacebookApi implements LoadFacebookUserApi {
         grant_type: 'client_credentials'
       }
     })
+  }
 
-    const debugToken = await this.httpClient.get({
+  private async getDebugToken (clientToken: string): Promise<DebugToken> {
+    const appToken = await this.getAppToken()
+
+    return this.httpClient.get({
       url: `${this.baseUrl}/debug_token`,
       params: {
         access_token: appToken.access_token,
-        input_token: params.token
+        input_token: clientToken
       }
     })
+  }
 
-    const userInfo = await this.httpClient.get({
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  private async getUserInfo (clientToken: string): Promise<UserInfo> {
+    const debugToken = await this.getDebugToken(clientToken)
+
+    return this.httpClient.get({
       url: `${this.baseUrl}/${debugToken.data.user_id}`,
       params: {
         fields: ['id', 'name', 'email'].join(','),
-        access_token: params.token
+        access_token: clientToken
       }
     })
-
-    return {
-      email: userInfo.email,
-      facebookId: userInfo.id,
-      name: userInfo.name
-    }
   }
 }
