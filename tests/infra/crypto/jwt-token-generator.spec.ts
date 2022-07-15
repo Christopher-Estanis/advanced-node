51c93d175285b1ae555345126671944692df5f99
christopher-estanis <christopher.estanis@gmail.com>
2022-07-14 22:27:55 -0300

    ensure JwtTokenGenerator returns token on success

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -19,18 +19,34 @@ describe('JwtTokenGenerator', () => {
   let sut: JwtTokenGenerator
   let fakeJwt: jest.Mocked<typeof jwt>
 
+  let key: object
+  let expirationInMs: number
+  let secret: string
+  let token: string
+
   beforeAll(() => {
+    key = { any: 'any_key' }
+    expirationInMs = 1000
+    secret = 'any_secret'
+
     fakeJwt = jwt as jest.Mocked<typeof jwt>
+    fakeJwt.sign.mockImplementation(() => token)
   })
 
   beforeEach(() => {
-    sut = new JwtTokenGenerator('any_secret')
+    sut = new JwtTokenGenerator(secret)
   })
 
   it('should call sign with correct params', async () => {
-    await sut.generateToken({ key: { any: 'any_key' }, expirationInMs: 1000 })
+    await sut.generateToken({ key, expirationInMs })
 
-    expect(fakeJwt.sign).toHaveBeenCalledWith({ any: 'any_key' }, 'any_secret', { expiresIn: 1 })
+    expect(fakeJwt.sign).toHaveBeenCalledWith(key, secret, { expiresIn: 1 })
     expect(fakeJwt.sign).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return token on success', async () => {
+    const result = await sut.generateToken({ key, expirationInMs })
+
+    expect(result).toEqual(token)
   })
 })
