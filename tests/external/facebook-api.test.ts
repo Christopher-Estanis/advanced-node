1c37e56991b52a48a7e1b49cc62921c605e0d78c
christopher-estanis <christopher.estanis@gmail.com>
2022-08-24 22:19:34 -0300

    clean a Facebook API test

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -3,11 +3,16 @@ import { AxiosHttpClient } from '@/infra/http'
 import { env } from '@/main/config/env'
 
 describe('Facebook API Integration Tests', () => {
-  it('should return a Facebook User if token is valid', async () => {
-    const axiosClient = new AxiosHttpClient()
-    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
+  let axiosClient: AxiosHttpClient
+  let sut: FacebookApi
 
-    // TOKEN VALIDATE IN 3 MONTHS - 24/08/22
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient()
+    sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
+  })
+
+  it('should return a Facebook User if token is valid', async () => {
+    // [INFO] TOKEN EXPIRES IN 3 MONTHS - 24/08/22
     const fbUser = await sut.loadUser({ token: 'EAAFnkT3gDg8BACjI5y2mNU88mZAGZA4xvu4NJvsdrXJ1WhNHZAhmGCZAhmZCMPMBbAWHM9hin9xkOHXjeJ0TmFsgTfZBjsRU7naXSRDfg6jlVgrAX9VkioSzAj4JFoSFP1Lh2hMQhyAIZBtrtwZBprkbpUAQuZAZBEZBzqxtee8OT12jQiZC24Rq68RMxxK3OaZCoPZCdz0hnIAWWbYNiFZBhtIlxmc' })
 
     expect(fbUser).toEqual({
@@ -18,9 +23,6 @@ describe('Facebook API Integration Tests', () => {
   })
 
   it('should return undefined token is invalid', async () => {
-    const axiosClient = new AxiosHttpClient()
-    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
-
     // TOKEN VALIDATE IN 3 MONTHS - 24/08/22
     const fbUser = await sut.loadUser({ token: 'undefined' })
 
